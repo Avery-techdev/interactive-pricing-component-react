8b0e6d0771c783c2404c6ddf9d881502502a21d5
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-04-23 17:18:02 +0200

    build background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App Container">
+      <div className="header">
+      <h2>
+        Simple, traffic-based pricing
+      </h2>
+      <p className="grey-text">
+        Sign-up for our 30-day trial. No credit card required.
+      </p>
+      </div>
+      <footer>
+       Challenge by <a href="https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8" target="_blank" >Frontend Mentor</a> <br /> <a href="https://github.com/Avery-techdev/interactive-pricing-component-react" target="_blank" >coded</a> by Annabell Hauschild
+      </footer>
     </div>
   );
 }
-
-export default App;
